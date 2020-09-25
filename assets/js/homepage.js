f441d9e3c68370dbd7c711161215ed926b85e6ed
vkfbmzef <monkeylordjunior@gmail.com>
2020-09-24 17:14:43 -0700

    language buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 
 
@@ -43,6 +44,21 @@ var getUserRepos = function(user) {
 
     console.log(event);
   };
+
+  var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+          } else {
+            alert("Error: " + response.statusText);
+      }
+    });
+  };
+
 
   var displayRepos = function(repos, searchTerm) {
     // check if api returned any repos
@@ -98,5 +114,21 @@ var getUserRepos = function(user) {
 
 
 
+var buttonClickHandler = function(event) {
+        var language = event.target.getAttribute("data-language");
+        console.log(language);
+
+        if (language) {
+            getFeaturedRepos(language);
+          
+            // clear old content
+            repoContainerEl.textContent = "";
+          }
+    };
+
+
+
 
   userFormEl.addEventListener("submit", formSubmitHandler);
+
+  languageButtonsEl.addEventListener("click", buttonClickHandler);
